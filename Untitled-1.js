fd989b19bb43e55ca72c03c0a896f82348210771
CarlosZBent <carlosezb981109@gmail.com>
2021-12-07 15:42:21 -0500

    positive action buttons create the skeleton for the tasks, but dont save it to local storage and have no style or interaction

--- Untitled-1.js
+++ Untitled-1.js
@@ -25,27 +25,38 @@ let todoBt = document.getElementById('todoBt')
 let typeOfTaskSection = document.getElementById('typeOfTaskSectionId')
 
 // negative action buttons and container
-let recycleBinBt = document.getElementById('recycleBinBt')
-let somedayMaybeBt = document.getElementById('somedayMaybeBt')
-let referenceMaterialBt = document.getElementById('referenceMaterialBt')
-let negativeActionSection = document.getElementById('negativeActionSectionId')
+let recycleBinBt = document.getElementById('recycleBinBt');
+let somedayMaybeBt = document.getElementById('somedayMaybeBt');
+let referenceMaterialBt = document.getElementById('referenceMaterialBt');
+let negativeActionSection = document.getElementById('negativeActionSectionId');
 
-let finalActionContainer = document.getElementById('finalActionContainerId')
-
-const showFinalSection = function () {
-  let finalActionSubContainer = document.createElement('div');
-  finalActionContainer.append(finalActionSubContainer);
-  let finalTaskUl = document.createElement('ul');
-  finalActionSubContainer.append(finalTaskUl);
-  let finalTaskLi = document.createElement('li');
-  finalTaskUl.append(finalTaskLi);
-  let finalListItems = document.createTextNode('test')
-  finalTaskLi.append(finalListItems)
+// new project
+let projectsContainer = document.getElementById('projectsContainerId');
+const saveNewProject = function () {
+  let projectsSubContainer = document.createElement('div');
+  projectsContainer.append(projectsSubContainer);
+  let projectBody = document.createElement('p');
+  let projectBodyText = document.createTextNode('test project body text');
+  projectBody.append(projectBodyText);
+  projectsSubContainer.append(projectBody);
 }
+
+// new todo
+let todosContainer = document.getElementById('todosContainerId');
+let todosParentList = document.getElementById('todosParentListId');
+const saveNewTodo = function () {
+  let newTodo = document.createElement('li');
+  let newTodoText = document.createTextNode('test todo text');
+  newTodo.append(newTodoText);
+  todosParentList.append(newTodo);
+}
+
+// show links to external software
+let externalSoftwareLinksSection = document.getElementById('externalSoftwareLinksSectionId');
 
 // ***add text to container***
 
-//getting DOM elements
+// getting DOM elements
 let taskNameInput = document.getElementById("finalText");
 let secondForm = document.getElementById('secondForm')
 
